1b87cb26d6ff2809cda6dcdfba2e45e5e21f876a
Mariya <Mashyly1994@gmail.com>
2021-09-05 19:23:58 +0300

    added text box tests

---
+++ src/test/java/ru/desnol/tests/TextBoxTests.java
@@ -0,0 +1,32 @@
+package ru.desnol.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+     static void beforeAll(){
+                Configuration.startMaximized=true;
+    }
+    @Test
+    void fillFormTest() {
+        String permanentAddress = "another street,1";
+
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Mariya");
+        $("#userEmail").setValue("Mashyly1994@gmail.com");
+        $("#currentAddress").setValue("Lermontova street,1");
+        $("#permanentAddress").setValue("another street,1");
+        $("#submit").scrollTo().click();
+
+        $("#output #name").shouldHave(text("Mariya"));
+        $("#output #email").shouldHave(text("Mashyly1994@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("Lermontova street,1"));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
+}
