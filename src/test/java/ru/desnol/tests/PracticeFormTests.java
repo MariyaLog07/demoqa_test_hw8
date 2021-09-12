84fc4874dbca17f83df4e5f059c8a1708a4b6fcb
Mariya <Mashyly1994@gmail.com>
2021-09-12 14:11:44 +0300

    added format code

--- src/test/java/ru/desnol/tests/PracticeFormTests.java
+++ src/test/java/ru/desnol/tests/PracticeFormTests.java
@@ -13,11 +13,12 @@ import static com.codeborne.selenide.Selenide.*;
 public class PracticeFormTests {
 
     @BeforeAll
-static void beforeAll() {
-    Configuration.startMaximized = true;
-}
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
     @Test
-    void fillPracticeFormTest(){
+    void fillPracticeFormTest() {
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue("Mariya");
         $("#lastName").setValue("Logutenko");
@@ -39,10 +40,13 @@ static void beforeAll() {
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Mariya Logutenko"),text("Test@gmail.com"),text("Female"),text("8900369852"),text("25 August,2020"),text("Maths"),text("Sports"),text("1.png"),text("Lermontova street,1"),text("NCR Delhi" ));
+        $(".table-responsive").shouldHave(
+                text("Mariya Logutenko"), text("Test@gmail.com"), text("Female"),
+                text("8900369852"), text("25 August,2020"), text("Maths"),
+                text("Sports"), text("1.png"), text("Lermontova street,1"), text("NCR Delhi"));
     }
 
-    }
+}
 
 
 
